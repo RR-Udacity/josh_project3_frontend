080d6d2e50b7baefa0dad70e903456f58e4daab0
Josh Haines <Josh@JoshHaines.com>
2021-07-06 23:36:45 -0400

    working on refactor

--- app/routes.py
+++ app/routes.py
@@ -1,66 +1,81 @@
-from app import app, db, queue_client
+from app import app, db
 from datetime import datetime
 from app.models import Attendee, Conference, Notification
-from flask import render_template, session, request, redirect, url_for, flash, make_response, session
-from azure.servicebus import Message
+from flask import (
+    render_template,
+    session,
+    request,
+    redirect,
+    url_for,
+    flash,
+    make_response,
+    session,
+)
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 import logging
+from icecream import ic
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/Registration', methods=['POST', 'GET'])
+@app.route("/Registration", methods=["POST", "GET"])
 def registration():
-    if request.method == 'POST':
+    if request.method == "POST":
         attendee = Attendee()
-        attendee.first_name = request.form['first_name']
-        attendee.last_name = request.form['last_name']
-        attendee.email = request.form['email']
-        attendee.job_position = request.form['job_position']
-        attendee.company = request.form['company']
-        attendee.city = request.form['city']
-        attendee.state = request.form['state']
-        attendee.interests = request.form['interest']
-        attendee.comments = request.form['message']
-        attendee.conference_id = app.config.get('CONFERENCE_ID')
+        attendee.first_name = request.form["first_name"]
+        attendee.last_name = request.form["last_name"]
+        attendee.email = request.form["email"]
+        attendee.job_position = request.form["job_position"]
+        attendee.company = request.form["company"]
+        attendee.city = request.form["city"]
+        attendee.state = request.form["state"]
+        attendee.interests = request.form["interest"]
+        attendee.comments = request.form["message"]
+        attendee.conference_id = app.config.get("CONFERENCE_ID")
 
         try:
             db.session.add(attendee)
             db.session.commit()
-            session['message'] = 'Thank you, {} {}, for registering!'.format(attendee.first_name, attendee.last_name)
-            return redirect('/Registration')
+            session["message"] = "Thank you, {} {}, for registering!".format(
+                attendee.first_name, attendee.last_name
+            )
+            return redirect("/Registration")
         except:
-            logging.error('Error occured while saving your information')
+            logging.error("Error occured while saving your information")
 
     else:
-        if 'message' in session:
-            message = session['message']
-            session.pop('message', None)
-            return render_template('registration.html', message=message)
+        if "message" in session:
+            message = session["message"]
+            session.pop("message", None)
+            return render_template("registration.html", message=message)
         else:
-             return render_template('registration.html')
+            return render_template("registration.html")
 
-@app.route('/Attendees')
+
+@app.route("/Attendees")
 def attendees():
     attendees = Attendee.query.order_by(Attendee.submitted_date).all()
-    return render_template('attendees.html', attendees=attendees)
+    return render_template("attendees.html", attendees=attendees)
 
 
-@app.route('/Notifications')
+@app.route("/Notifications")
 def notifications():
     notifications = Notification.query.order_by(Notification.id).all()
-    return render_template('notifications.html', notifications=notifications)
+    return render_template("notifications.html", notifications=notifications)
 
-@app.route('/Notification', methods=['POST', 'GET'])
+
+@app.route("/Notification", methods=["POST", "GET"])
 def notification():
-    if request.method == 'POST':
+    if request.method == "POST":
         notification = Notification()
-        notification.message = request.form['message']
-        notification.subject = request.form['subject']
-        notification.status = 'Notifications submitted'
+        notification.message = request.form["message"]
+        notification.subject = request.form["subject"]
+        notification.status = "Notifications submitted"
         notification.submitted_date = datetime.utcnow()
 
         try:
@@ -71,37 +86,58 @@ def notification():
             ## TODO: Refactor This logic into an Azure Function
             ## Code below will be replaced by a message queue
             #################################################
-            attendees = Attendee.query.all()
+            # attendees = Attendee.query.all()
 
-            for attendee in attendees:
-                subject = '{}: {}'.format(attendee.first_name, notification.subject)
-                send_email(attendee.email, subject, notification.message)
+            # for attendee in attendees:
+            #     subject = '{}: {}'.format(attendee.first_name, notification.subject)
+            #     send_email(attendee.email, subject, notification.message)
 
-            notification.completed_date = datetime.utcnow()
-            notification.status = 'Notified {} attendees'.format(len(attendees))
-            db.session.commit()
+            # notification.completed_date = datetime.utcnow()
+            # notification.status = 'Notified {} attendees'.format(len(attendees))
+            # db.session.commit()
             # TODO Call servicebus queue_client to enqueue notification ID
+            connection_string = os.environ["SERVICE_BUS_CONNECTION_STRING"]
+            ic(connection_string)
+            queue_name = "techqueue"
+            servicebus_client = ServiceBusClient.from_connection_string(
+                conn_str=connection_string, logging_enable=True
+            )
+            ic(servicebus_client)
 
+            with servicebus_client:
+                sender = servicebus_client.get_queue_sender(queue_name=queue_name)
+                ic(sender)
+                with sender:
+                    send_single_message(sender, notification.id)
             #################################################
             ## END of TODO
             #################################################
-
-            return redirect('/Notifications')
-        except :
-            logging.error('log unable to save notification')
+            ic()
+            return redirect("/Notifications")
+        except:
+            logging.error("log unable to save notification")
 
     else:
-        return render_template('notification.html')
-
+        return render_template("notification.html")
 
 
 def send_email(email, subject, body):
-    if not app.config.get('SENDGRID_API_KEY'):
+    if not app.config.get("SENDGRID_API_KEY"):
         message = Mail(
-            from_email=app.config.get('ADMIN_EMAIL_ADDRESS'),
+            from_email=app.config.get("ADMIN_EMAIL_ADDRESS"),
             to_emails=email,
             subject=subject,
-            plain_text_content=body)
+            plain_text_content=body,
+        )
 
-        sg = SendGridAPIClient(app.config.get('SENDGRID_API_KEY'))
+        sg = SendGridAPIClient(app.config.get("SENDGRID_API_KEY"))
         sg.send(message)
+
+
+def send_single_message(sender, notification_id):
+    # create a Service Bus message
+    message = ServiceBusMessage(notification_id)
+    ic(message)
+    # send the message to the queue
+    sender.send_messages(message)
+    logging.info("Sent the notification id: {}".format(notification_id))
--- config.py
+++ config.py
@@ -2,20 +2,28 @@ import os
 
 app_dir = os.path.abspath(os.path.dirname(__file__))
 
+
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL=os.environ["POSTGRES_URL"]  #TODO: Update value
-    POSTGRES_USER="psql_admin@project3-db" #TODO: Update value
-    POSTGRES_PW=os.environ["POSTGRES_PW"]   #TODO: Update value
-    POSTGRES_DB=os.environ["POSTGRES_DB"]   #TODO: Update value
-    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    POSTGRES_URL = os.environ["POSTGRES_URL"]  # TODO: Update value
+    POSTGRES_USER = "psql_admin@project3-db"  # TODO: Update value
+    POSTGRES_PW = os.environ["POSTGRES_PW"]  # TODO: Update value
+    POSTGRES_DB = os.environ["POSTGRES_DB"]  # TODO: Update value
+    DB_URL = "postgresql://{user}:{pw}@{url}/{db}".format(
+        user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB
+    )
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI") or DB_URL
     CONFERENCE_ID = 1
-    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING =os.environ['SERVICE_BUS_CONNECTION_STRING'] #TODO: Update value
-    SERVICE_BUS_QUEUE_NAME ='joshnotificationqueue'
-    ADMIN_EMAIL_ADDRESS: 'Clark.Kent@joshhaines.com'
-    SENDGRID_API_KEY = os.environ['SENDGRID_API_KEY'] #Configuration not required, required SendGrid Account
+    SECRET_KEY = "LWd2tzlprdGHCIPHTd4tp5SBFgDszm"
+    SERVICE_BUS_CONNECTION_STRING = os.environ[
+        "SERVICE_BUS_CONNECTION_STRING"
+    ]  # TODO: Update value
+    SERVICE_BUS_QUEUE_NAME = "joshnotificationqueue"
+    ADMIN_EMAIL_ADDRESS: "Clark.Kent@joshhaines.com"
+    SENDGRID_API_KEY = os.environ[
+        "SENDGRID_API_KEY"
+    ]  # Configuration not required, required SendGrid Account
+
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
